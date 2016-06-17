efb0e5cdc06ba9ad5036a10e4e4d04f7e19efeaa
Matt Lee <mattlee68@users.noreply.github.com>
2016-06-18 11:38:19 +1200

    Update 2010-5c.c

--- 2010/2010-5c.c
+++ 2010/2010-5c.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argv, char **argc){
+int main(int argc, char **argv){
     int i;
     for(i = 1; i < argv; i++){//skips the file name at the start
         printf("%s\n", argc[i]);
